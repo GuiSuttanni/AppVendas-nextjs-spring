c4cbdd2ac72306bfb9c7d9090a6303d3904f2b61
Guilherme Suttanni <guilherme.ferreira@mv.com.br>
2022-08-29 21:53:41 -0300

    Ajustes em layout e alinhamentos dos campos

--- src/components/layout/menu/index.tsx
+++ src/components/layout/menu/index.tsx
@@ -2,9 +2,10 @@ import Link from 'next/link'
 
 export const Menu: React.FC = () => {
     return (
-        <aside className="column is-2 is-narrow-mobile is-fullhight section is-hidden-mobile">
-            <p className="menu-label is-hidden-touch">
-                Minhas vendas
+        <aside className="column is-2 is-narrow-mobile is-fullhight section is-hidden-mobile"
+            style={{borderRight: '1px solid'}}>
+            <p className="menu-label is-hidden-touch bold" style={{fontWeight:'bold',fontSize:'15px'}}>
+                VENDAS
             </p>
             <ul className="menu-list">
                 <MenuItem href="/" label="Home"/>
--- src/components/produtos/cadastro/index.tsx
+++ src/components/produtos/cadastro/index.tsx
@@ -3,39 +3,45 @@ import { Layout } from 'components';
 export const CadastroProdutos: React.FC = () => {
     return (
         <Layout titulo="Cadastro de Produtos">
-            <div className="field">
-                <label className="label" htmlFor="inputSku">SKU: *</label>
-                <div className="control">
-                    <input className="input" 
-                           id="inputSku"
-                           placeholder="Digite o SKU do produto" />
+            <div className="columns">
+                <div className="field is-half column">
+                    <label className="label" htmlFor="inputSku">Código: *</label>
+                    <div className="control">
+                        <input className="input" 
+                            id="inputSku"
+                            placeholder="Digite o CÓDIGO do produto" />
+                    </div>
+                </div>
+
+                <div className="field is-half column">
+                    <label className="label" htmlFor="inputPreco">Preço: *</label>
+                    <div className="control">
+                        <input className="input" 
+                            id="inputPreco"
+                            placeholder="Digite o PREÇO do produto" />
+                    </div>
                 </div>
            </div>
 
-           <div className="field">
-                <label className="label" htmlFor="inputPreco">Preço: *</label>
-                <div className="control">
-                    <input className="input" 
-                           id="inputPreco"
-                           placeholder="Digite o PREÇO do produto" />
+           <div className="columns"> 
+                <div className="field column is-full">
+                    <label className="label" htmlFor="inputNome">Nome: *</label>
+                    <div className="control">
+                        <input className="input" 
+                            id="inputNome"
+                            placeholder="Digite o NOME do produto" />
+                    </div>
                 </div>
            </div>
 
-           <div className="field">
-                <label className="label" htmlFor="inputNome">Nome: *</label>
-                <div className="control">
-                    <input className="input" 
-                           id="inputNome"
-                           placeholder="Digite o NOME do produto" />
-                </div>
-           </div>
-
-           <div className="field">
-                <label className="label" htmlFor="inputDesc">Descrição: *</label>
-                <div className="control">
-                    <textarea className="textarea" 
-                           id="inputDesc"
-                           placeholder="Digite a DESCRIÇÃO do produto" />
+           <div className="columns"> 
+                <div className="field column is-full">
+                    <label className="label" htmlFor="inputDesc">Descrição: *</label>
+                    <div className="control">
+                        <textarea className="textarea" 
+                            id="inputDesc"
+                            placeholder="Digite a DESCRIÇÃO do produto" />
+                    </div>
                 </div>
            </div>
 
