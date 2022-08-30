0771fe37ecf5e7a3effe2c675e9d0a32fcab49a7
Guilherme Suttanni <guilherme.ferreira@mv.com.br>
2022-08-29 21:18:32 -0300

    16. Criação do formulario dos produtos

--- src/components/produtos/cadastro/index.tsx
+++ src/components/produtos/cadastro/index.tsx
@@ -3,7 +3,50 @@ import { Layout } from 'components';
 export const CadastroProdutos: React.FC = () => {
     return (
         <Layout titulo="Cadastro de Produtos">
-            Aqui vai ficar o meu formulario
+            <div className="field">
+                <label className="label" htmlFor="inputSku">SKU: *</label>
+                <div className="control">
+                    <input className="input" 
+                           id="inputSku"
+                           placeholder="Digite o SKU do produto" />
+                </div>
+           </div>
+
+           <div className="field">
+                <label className="label" htmlFor="inputPreco">Preço: *</label>
+                <div className="control">
+                    <input className="input" 
+                           id="inputPreco"
+                           placeholder="Digite o PREÇO do produto" />
+                </div>
+           </div>
+
+           <div className="field">
+                <label className="label" htmlFor="inputNome">Nome: *</label>
+                <div className="control">
+                    <input className="input" 
+                           id="inputNome"
+                           placeholder="Digite o NOME do produto" />
+                </div>
+           </div>
+
+           <div className="field">
+                <label className="label" htmlFor="inputDesc">Descrição: *</label>
+                <div className="control">
+                    <textarea className="textarea" 
+                           id="inputDesc"
+                           placeholder="Digite a DESCRIÇÃO do produto" />
+                </div>
+           </div>
+
+           <div className="field is-grouped">
+                <div className="control">
+                    <button className="button is-link">Salvar</button>
+                </div>
+                <div className="control">
+                    <button className="button is-link is-light">Voltar</button>
+                </div>
+           </div>
         </Layout>
     )
 }
